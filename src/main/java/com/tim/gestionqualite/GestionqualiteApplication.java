4b27339f038b1d353d3024e5794077fe2c2ba01f
RebhiEya <eyarebhi29@gmail.com>
2024-03-09 16:52:05 +0100

    create project

---
+++ src/main/java/com/tim/gestionqualite/GestionqualiteApplication.java
@@ -0,0 +1,13 @@
+package com.tim.gestionqualite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionqualiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionqualiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim/gestionqualite/GestionqualiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.gestionqualite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionqualiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
